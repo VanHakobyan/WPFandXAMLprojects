3c31a18f844ebef96826859e0840b265d74b939f
VanHakobyan <van19962013@mail.ru>
2017-04-10 19:01:41 +0400

    add class generator answer and :feet:

---
+++ FirstStep/EightBall/AnswerGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightBall
+{
+   public static class AnswerGenerator
+    {
+        private static string[] answer = new string[]
+        {
+            "Կրկին փորձիր",
+            "Այո իհարկե",
+            "Այո",
+
+            "Չեմ կողմնորոշվում",
+            "Օ ինչ եք ասում?",
+            "Կտեսնենք",
+            "Դեռ ոչ",
+            "Հնարավոր է",
+            "Ոչ",
+            "Միանշանակ",
+            "Ինչու ոչ",
+            "Փործել է պետք",
+            "Չեմ կարծում"
+
+        };
+        public static string GetRandomAnswer()
+        {
+            return answer[new Random().Next(answer.Length)];
+        }
+    }
+}
