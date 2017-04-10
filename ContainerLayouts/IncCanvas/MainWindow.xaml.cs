bd963562c35611e04e97a803568216be9417107c
VanHakobyan <van19962013@mail.ru>
2017-04-10 23:08:39 +0400

    add IncCanvas :pencil2:

---
+++ ContainerLayouts/IncCanvas/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IncCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void INK_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+            string s = string.Empty;
+            foreach (var res in e.GetGestureRecognitionResults())
+            {
+                s += res.ApplicationGesture.ToString() + "; ";
+            }
+            Guest.Name = s;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var mode in Enum.GetValues(typeof(InkCanvasEditingMode)))
+            {
+                Combo.Items.Add(mode);
+            }
+            Combo.SelectedItem = INK.EditingMode;
+        }
+    }
+}
