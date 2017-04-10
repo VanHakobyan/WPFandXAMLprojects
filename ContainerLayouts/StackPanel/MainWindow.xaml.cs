50c3decc8fa0535f8de1fa8e3df18960126efab1
VanHakobyan <van19962013@mail.ru>
2017-04-10 21:43:34 +0400

    changed  :movie_camera:

---
+++ ContainerLayouts/StackPanel/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StackPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = Brushes.Brown;
+        }
+
+        private void textBlock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = Brushes.Yellow;
+        }
+    }
+}
