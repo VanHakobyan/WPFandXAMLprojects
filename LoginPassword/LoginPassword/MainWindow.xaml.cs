e9aef795f455480100c317bb1b6280b5193b1321
vanhakobyan <van19962013@mail.ru>
2017-04-11 16:20:25 +0400

    add Login Password

--- LoginPassword/LoginPassword/MainWindow.xaml.cs
+++ LoginPassword/LoginPassword/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,12 +18,25 @@ namespace LoginPassword
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// 
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Login.Text == string.Empty && Password.Password == string.Empty) ||( Password.Password.Length <= 6 && Password.Password.Length >= 15))
+            {
+                MessageBox.Show("Please correctly enter the Login");
+            }
+            else
+            {
+                MessageBox.Show($"Hello {Login.Text} i miss you bebe !!!");
+            }
         }
     }
 }
