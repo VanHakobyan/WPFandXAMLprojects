d784be803a4801d9c18b1c84e2e77e16255f8315
VanHakobyan <van19962013@mail.ru>
2017-04-11 21:57:32 +0400

    add  button and panel :metal:

--- DataBinding/elementTOelement/MainWindow.xaml.cs
+++ DataBinding/elementTOelement/MainWindow.xaml.cs
@@ -24,5 +24,20 @@ namespace elementTOelement
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
