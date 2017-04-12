c696c5a70ed6ee7dd182f98b2b794f56c9bdb1bc
VanHakobyan <van19962013@mail.ru>
2017-04-12 21:07:50 +0400

    add Player.cs and Team :soccer:

---
+++ BindingWPFtoDataBase/BindingWPFtoDataBase/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingWPFtoDataBase
+{
+    public class Player
+    {
+        public string NamePlayer { get; set; }
+        public string About { get; set; }
+        public Player()
+        {
+
+        }
+        public Player(string namePlayer,string about)
+        {
+            NamePlayer = namePlayer;
+            About = about;
+        }
+    }
+}
