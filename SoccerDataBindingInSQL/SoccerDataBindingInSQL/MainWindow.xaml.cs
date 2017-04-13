215a79825f5f7801e358cc174c3e2628d661cc30
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-13 16:14:58 +0400

    add show in number :rain:

--- SoccerDataBindingInSQL/SoccerDataBindingInSQL/MainWindow.xaml.cs
+++ SoccerDataBindingInSQL/SoccerDataBindingInSQL/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,37 @@ namespace SoccerDataBindingInSQL
         private void Author_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Created by VH Production LLC");
+        }
+
+        private void ShowInNumber_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlDataReader reader = null;
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConnectionString))
+            {
+
+                try
+                {
+                    int PlayerID = int.Parse(Namber.Text);
+                    SqlCommand command = new SqlCommand("SELECT * FROM [dbo].[Table] WHERE ID=@id", connection);
+                    command.Parameters.AddWithValue("id", PlayerID);
+                    connection.Open();
+
+                    reader = command.ExecuteReader();
+                    reader.Read();
+
+                    FirstLastNAme.Text = reader[1].ToString();
+                    AboutIn.Text = reader[2].ToString();
+
+                    reader.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Imput Corcect number !!!");
+                }
+
+
+            }
         }
     }
 }
